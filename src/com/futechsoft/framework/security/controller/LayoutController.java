a55114a64a0d2f840007840b17adbb07f144564a
hyuny00 <10jqk1@kakao.com>
2025-03-31 10:56:02 +0900

    edit menu layout

--- src/com/futechsoft/framework/security/controller/LayoutController.java
+++ src/com/futechsoft/framework/security/controller/LayoutController.java
@@ -1,20 +1,16 @@
 package com.futechsoft.framework.security.controller;
 
 import java.util.List;
-import java.util.Map;
 
-import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import com.futechsoft.admin.menu.vo.Menu;
 import com.futechsoft.framework.security.service.LayoutService;
 import com.futechsoft.framework.security.vo.MenuVO;
 
@@ -25,19 +21,18 @@ public class LayoutController {
 	@Autowired
 	private LayoutService layoutService;
 
-	/*
 	@RequestMapping("/framework/layout/header")
 	public String topMenu(HttpServletRequest request) throws Exception {
 
-		request.setAttribute("topMenuSeqList", layoutService.getTopMenuSeqList());
-		request.setAttribute("menuList", layoutService.getMenuListMap());
+	//	request.setAttribute("topMenuSeqList", layoutService.getTopMenuSeqList());
+	//	request.setAttribute("menuList", layoutService.getMenuListMap());
 
 		return "framework/layout/header";
 	}
 
 	@RequestMapping("/framework/layout/leftMenu")
 	public String leftMenu(HttpServletRequest request) throws Exception {
-
+/*
 		Map<String, List<Menu>> menuListMap = layoutService.getMenuListMap();
 
 		request.setAttribute("authMenuList", layoutService.getAuthMenuList());
@@ -46,22 +41,22 @@ public class LayoutController {
 
 
 		request.setAttribute("menuList", layoutService.getMenuListMap());
-
+*/
 		return "framework/layout/leftMenu";
 	}
 
 	@RequestMapping("/framework/layout/adminHeader")
 	public String adminHeader(HttpServletRequest request) throws Exception {
 
-		request.setAttribute("topMenuSeqList", layoutService.getTopMenuSeqList());
-		request.setAttribute("menuList", layoutService.getMenuListMap());
+		//request.setAttribute("topMenuSeqList", layoutService.getTopMenuSeqList());
+		//request.setAttribute("menuList", layoutService.getMenuListMap());
 
 		return "framework/layout/adminHeader";
 	}
 
 	@RequestMapping("/framework/layout/adminLeftMenu")
 	public String adminLeftMenu(HttpServletRequest request) throws Exception {
-
+/*
 		request.setAttribute("authMenuList", layoutService.getAuthMenuList());
 
 		Map<String, List<Menu>> menuListMap = layoutService.getMenuListMap();
@@ -70,10 +65,9 @@ public class LayoutController {
 		request.setAttribute("subMenuList", menuListMap.get(topMenuSeq));
 
 		request.setAttribute("menuList", layoutService.getMenuListMap());
-
+*/
 		return "framework/layout/adminLeftMenu";
 	}
-	*/
 	
 	@GetMapping("/framework/layout/authMenuTree")
     public ResponseEntity<List<MenuVO>> getAuthMenuTree(@RequestParam(required = false) Long selectedMenuSeq) {
